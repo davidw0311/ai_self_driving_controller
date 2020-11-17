4196f83e983d1284a52f6403eb3550778d611217
Sylvia <sylvia.zhang@alumni.ubc.ca>
2020-11-16 19:26:13 -0800

    made run_sim.sh file in controller to launch the sim more conveniently

--- controller.sh
+++ controller.sh
@@ -1,5 +1,4 @@
+#!/bin/bash
 source ~/ros_ws/devel/setup.bash
 # gnome-terminal -e  
-cd ~/ros_ws/src/2020T1_competition/enph353/enph353_utils/scripts
-./run_sim.sh
 roslaunch my_controller process_image.launch
---
+++ run_sim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ~/ros_ws/devel/setup.bash
+cd ~/ros_ws/src/2020T1_competition/enph353/enph353_utils/scripts
+./run_sim.sh
